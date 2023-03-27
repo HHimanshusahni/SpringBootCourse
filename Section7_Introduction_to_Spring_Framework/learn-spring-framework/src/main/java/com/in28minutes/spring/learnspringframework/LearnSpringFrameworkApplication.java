5ecaf545fdcf67462c628722506bc28426f1d71b
Himanshu Sahni <sahnihimanshu97@gmail.com>
2023-03-27 20:51:11 +0530

    Iteration 3 Loose coupling Level 2 - Spring framework

--- Section7_Introduction_to_Spring_Framework/learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/LearnSpringFrameworkApplication.java
+++ Section7_Introduction_to_Spring_Framework/learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/LearnSpringFrameworkApplication.java
@@ -1,9 +1,12 @@
 package com.in28minutes.spring.learnspringframework;
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 import com.in28minutes.spring.learnspringframework.game.GameRunner;
+import com.in28minutes.spring.learnspringframework.game.GamingConsole;
 import com.in28minutes.spring.learnspringframework.game.MarioGame;
 import com.in28minutes.spring.learnspringframework.game.PacmanGame;
 import com.in28minutes.spring.learnspringframework.game.SuperContraGame;
@@ -12,12 +15,17 @@ import com.in28minutes.spring.learnspringframework.game.SuperContraGame;
 public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
-//		SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+		
+		// This line of code creates spring application context
+		ConfigurableApplicationContext context 
+				= SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+		GameRunner runner = context.getBean(GameRunner.class);
+		
 //		 We can use any of the games MarioGame or super contra without making changes to any of the classes
 //		MarioGame game = new MarioGame();
 //		SuperContraGame game = new SuperContraGame();
-		PacmanGame game = new PacmanGame();
-		GameRunner runner = new GameRunner(game);
+//		GamingConsole game = new PacmanGame(); // Step1
+//		GameRunner runner = new GameRunner(game); // Step2
 		runner.run();
 	}
 
