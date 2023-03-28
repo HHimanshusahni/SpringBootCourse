ce072b0e3ce5e1a10a09751d1e2e655526707173
Himanshu Sahni <sahnihimanshu97@gmail.com>
2023-03-28 21:45:17 +0530

    When there are mulitiple components there is an error

--- Section7_Introduction_to_Spring_Framework/learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/game/SuperContraGame.java
+++ Section7_Introduction_to_Spring_Framework/learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/game/SuperContraGame.java
@@ -2,7 +2,7 @@ package com.in28minutes.spring.learnspringframework.game;
 
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class SuperContraGame implements GamingConsole{
 	public void up() {
 		System.out.println("SuperContraGame up");
