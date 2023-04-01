780bfefd647dede676c0fe3dac292b0e443fe171
Himanshu Sahni <sahnihimanshu97@gmail.com>
2023-04-01 11:25:37 +0530

    using the devtools we don't need to manually restart the server every time the changes will be automatically reflected. But if we make changes in the pom.xml we need to restart the server

--- Section2_Introduction_toSpring_Boot/learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
+++ Section2_Introduction_toSpring_Boot/learn-spring-boot/src/main/java/com/in28minutes/springboot/learnspringboot/CourseController.java
@@ -10,12 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
-	
+		
 	@RequestMapping("/courses")
 	public List<Course> reteriveAllCourses() {
 		return Arrays.asList(
 				new Course(1,"Learn AWS","in28minutes"),
-				new Course(2,"Learn DevOps","in28minutes"));
+				new Course(2,"Learn DevOps","in28minutes"),
+				new Course(3,"Learn Azure","in28minutes"));
 		
 	}
 
